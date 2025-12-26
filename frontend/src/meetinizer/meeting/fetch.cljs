f69f35e5a0f1b67250007e8701424da75996733d
Kenneth Hedman <github@vitberget.se>
2025-12-26 11:47:37 +0100

    adapted to backend api changes

--- frontend/src/meetinizer/meeting/fetch.cljs
+++ frontend/src/meetinizer/meeting/fetch.cljs
@@ -1,5 +1,6 @@
 (ns meetinizer.meeting.fetch
-  (:require [meetinizer.the-state :refer [state-atom]]))
+  (:require
+    [meetinizer.the-state :refer [state-atom]]))
 
 (defn fetch-meeting [id]
   (-> (js/fetch (str "/api/meeting/" id))
@@ -9,8 +10,22 @@
                    403 (swap! state-atom assoc-in [:meeting id] :forbidden)
                    (swap! state-atom assoc-in [:meeting id] {:title "Hello"})))))))
 
+(defn fetch-whoami [id]
+  (-> (js/fetch (str "/api/meeting/" id "/whoami"))
+      (.then (fn [the-result]
+               (let [status (.-status the-result)]
+                 (prn "status" status)
+                 (condp = status
+                   403 (swap! state-atom assoc-in [:whoami id] :forbidden)
+                   (do
+                     (-> (.text the-result)
+                         (.then (fn [body]
+                                  (swap! state-atom assoc-in [:whoami id] body)
+                                  ))
+                         ))))))))
+
 (defn login [id email]
-  (-> (js/fetch (str "/api/login/request/" id "/" email))
+  (-> (js/fetch (str "/api/meeting/" id "/request-login/" email))
       (.then (fn [the-result]
                (let [status (.-status the-result)]
                  (condp = status
@@ -20,4 +35,6 @@
 (comment
   (fetch-meeting "123")
   (login "123" "kalle")
+  (fetch-whoami "555")
+  @state-atom
   )
