e848b39c319a4672331db44d6fee49c0ccd8fd1e
Kenneth Hedman <github@vitberget.se>
2025-12-26 12:00:50 +0100

    fetch real meeting

--- frontend/src/meetinizer/meeting/fetch.cljs
+++ frontend/src/meetinizer/meeting/fetch.cljs
@@ -1,40 +1,49 @@
 (ns meetinizer.meeting.fetch
-  (:require
-    [meetinizer.the-state :refer [state-atom]]))
+  (:require [meetinizer.the-state :refer [state-atom]]))
 
 (defn fetch-meeting [id]
   (-> (js/fetch (str "/api/meeting/" id))
       (.then (fn [the-result]
                (let [status (.-status the-result)]
                  (condp = status
+                   200 (-> (.json the-result)
+                           (.then (fn [json]
+                                    (let [data (js->clj json :keywordize-keys true)]
+                                      (swap! state-atom assoc-in [:meeting id] data)))))
+
                    403 (swap! state-atom assoc-in [:meeting id] :forbidden)
-                   (swap! state-atom assoc-in [:meeting id] {:title "Hello"})))))))
+
+                   (swap! state-atom assoc-in [:meeting id] :error)))))))
 
 (defn fetch-whoami [id]
   (-> (js/fetch (str "/api/meeting/" id "/whoami"))
       (.then (fn [the-result]
                (let [status (.-status the-result)]
-                 (prn "status" status)
                  (condp = status
+                   200 (-> (.text the-result)
+                           (.then (fn [body]
+                                    (swap! state-atom assoc-in [:whoami id] body))))
+
                    403 (swap! state-atom assoc-in [:whoami id] :forbidden)
-                   (do
-                     (-> (.text the-result)
-                         (.then (fn [body]
-                                  (swap! state-atom assoc-in [:whoami id] body)
-                                  ))
-                         ))))))))
+
+                   (swap! state-atom assoc-in [:whoami id] :error)))))))
 
 (defn login [id email]
   (-> (js/fetch (str "/api/meeting/" id "/request-login/" email))
       (.then (fn [the-result]
                (let [status (.-status the-result)]
                  (condp = status
+                   ; TODO show seconds left
+                   200 (swap! state-atom assoc-in [:meeting id] :requested)
+
                    403 (swap! state-atom assoc-in [:meeting id] :forbidden)
-                   (swap! state-atom assoc-in [:meeting id] :requested)))))))
+
+                   (swap! state-atom assoc-in [:meeting id] :error)))))))
 
 (comment
-  (fetch-meeting "123")
+  (fetch-meeting "777")
   (login "123" "kalle")
-  (fetch-whoami "555")
+  (fetch-whoami "777")
   @state-atom
+  (swap! state-atom assoc :whoami nil)
   )
