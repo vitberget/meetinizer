2f2c8686aba795552a6332d7fedd171e8a3f8733
Kenneth Hedman <github@vitberget.se>
2025-12-27 12:01:39 +0100

    new mock

--- backend/src/server/meeting/mod.rs
+++ backend/src/server/meeting/mod.rs
@@ -72,10 +72,15 @@ pub fn get_meeting_mock(id: &str) -> Meeting {
     let mut meeting = Meeting::new(format!("Hello {id}"));
     meeting.add_user(User::new("Kenneth Hedman", "test@vitberget.se"));
     let slot = Slot { 
-        start: NaiveDate::from_ymd_opt(2025, 6, 4).unwrap().and_hms_opt(15,0,0).unwrap().and_local_timezone(Local).earliest().unwrap(),
-        end: NaiveDate::from_ymd_opt(2025, 6, 4).unwrap().and_hms_opt(15,0,0).unwrap().and_local_timezone(Local).earliest().unwrap(),
+        start: NaiveDate::from_ymd_opt(2025, 6, 4).unwrap().and_hms_opt(18,0,0).unwrap().and_local_timezone(Local).earliest().unwrap(),
+        end: NaiveDate::from_ymd_opt(2025, 6, 4).unwrap().and_hms_opt(21,0,0).unwrap().and_local_timezone(Local).earliest().unwrap(),
+    };
+    let slot2 = Slot { 
+        start: NaiveDate::from_ymd_opt(2025, 6, 7).unwrap().and_hms_opt(18,0,0).unwrap().and_local_timezone(Local).earliest().unwrap(),
+        end: NaiveDate::from_ymd_opt(2025, 6, 7).unwrap().and_hms_opt(21,0,0).unwrap().and_local_timezone(Local).earliest().unwrap(),
     };
     meeting.add_slot(slot.clone());
+    meeting.add_slot(slot2);
     meeting.add_vote(User::new("Kenneth Hedman", "test@vitberget.se"), slot).unwrap();
     meeting
 }
