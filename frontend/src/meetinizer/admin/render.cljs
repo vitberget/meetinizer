5a1d20a22e2f3813cd68cc73831c01a0252fbd76
Kenneth Hedman <github@vitberget.se>
2025-12-28 11:31:10 +0100

    fe admin meeting wip

--- frontend/src/meetinizer/admin/render.cljs
+++ frontend/src/meetinizer/admin/render.cljs
@@ -11,8 +11,7 @@
    [:h1 "Enter admin password"]
    [:input#login-email {:type "email"
                         :replicant/on-mount [[:db/assoc :admin/login-form-element :dom/node]]
-                        :on {:input [[:db/assoc :admin/login-form :event/target.value]]}
-                        }]
+                        :on {:input [[:db/assoc :admin/login-form :event/target.value]]}}]
    [:input {:type "button" 
             :value "Login as admin"
             :on {:click [[:admin/login [:db/get :admin/login-form]]]}}]])
@@ -23,7 +22,15 @@
    (if (empty? meeting-ids)
      "No meetings yet"   
      [:ul (->> meeting-ids
-               (map (fn[m] [:li m])))])])
+               (map (fn[m] [:li [:input {:type "button"
+                                         :value m
+                                         :on {:click [[:db/assoc :admin/selected-meeting m]]}}]])))])])
+
+(defn render-meeting [{active :admin/selected-meeting :as state}]
+  [:main.admin.meeting
+   [:h1 "You have chosen " active]
+   ]
+  )
 
 (defn render-admin [state]
   (let [meetings (:meeting-ids state)]
@@ -40,9 +47,16 @@
       (= :forbidden meetings)
       (render-login state)
 
+      (:admin/selected-meeting state)
+      (render-meeting state)
+       
       :else
       (render-list state)
 
       ) 
     )
+  )
+(comment
+  @state-atom
+  (swap! state-atom dissoc :admin/selected-meeting)
   )
